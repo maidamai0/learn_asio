8766d45029f0ff980c3e8f8cba26f368c7236755
tonghao.yuan <yuantonghao@gmail.com>
2019-10-01 19:17:35 +0800

    fix:fix timer expire

--- threads/thread.cpp
+++ threads/thread.cpp
@@ -19,8 +19,8 @@ void timer1_hander(asio::error_code ec) {
             << __FUNCTION__ << " cnt is " << cnt++ << std::endl;
 
   if (cnt < 10) {
-    timer1.async_wait(timer1_hander);
     timer1.expires_after(std::chrono::seconds(10));
+    timer1.async_wait(timer1_hander);
     std::thread([]() { io_context.run(); }).detach();
   }
 }
@@ -32,8 +32,8 @@ void timer2_handler(asio::error_code ec) {
             << __FUNCTION__ << " cnt is " << cnt++ << std::endl;
 
   if (cnt < 10) {
-    timer2.async_wait(timer2_handler);
     timer2.expires_after(std::chrono::seconds(100));
+    timer2.async_wait(timer2_handler);
 
     std::thread([]() { io_context.run(); }).detach();
   }
@@ -45,11 +45,11 @@ int main(int argc, char** argv) {
   std::cout << "This thread id is " << std::this_thread::get_id() << " "
             << __FUNCTION__ << std::endl;
 
-  timer1.async_wait(timer1_hander);
   timer1.expires_after(std::chrono::seconds(2));
+  timer1.async_wait(timer1_hander);
 
-  timer2.async_wait(timer2_handler);
   timer2.expires_after(std::chrono::seconds(2));
+  timer2.async_wait(timer2_handler);
 
   std::thread([]() { io_context.run(); }).join();
 }
