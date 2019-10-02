7d7cb2fc147dd401480681c3951a295253aeb3a0
tonghao.yuan <yuantonghao@gmail.com>
2019-10-02 11:47:50 +0800

    feat:add daytime server and client

---
+++ tutorials/daytime1.cpp
@@ -0,0 +1,46 @@
+#include <fmt/core.h>
+#include <fmt/ostream.h>
+#include <asio/connect.hpp>
+#include <asio/io_context.hpp>
+#include <asio/ip/tcp.hpp>
+
+#include <array>
+#include <iostream>
+#include <thread>
+
+int main(int argc, char** argv) {
+  try {
+    if (argc != 3) {
+      fmt::print(std::cerr, "Usage:daytime1 <host> <port>\n");
+      return 1;
+    }
+
+    asio::io_context io_ctx;
+    asio::ip::tcp::resolver resolver(io_ctx);
+
+    auto endpoints = resolver.resolve(argv[1], argv[2]);
+
+    asio::ip::tcp::socket socket(io_ctx);
+    asio::connect(socket, endpoints);
+
+    while (true) {
+      std::array<char, 128> buf{0};
+      asio::error_code ec;
+
+      size_t len = socket.read_some(asio::buffer(buf), ec);
+      if (ec == asio::error::eof) {
+        // complete
+        break;
+      } else if (ec) {
+        throw asio::system_error(ec);
+      }
+
+      std::cout.write(buf.data(), len);
+    }
+
+  } catch (const std::exception& e) {
+    fmt::print(std::cerr, "{}\n", e.what());
+  }
+
+  return 0;
+}
---
+++ tutorials/daytime2.cpp
@@ -0,0 +1,40 @@
+#include <fmt/core.h>
+#include <fmt/ostream.h>
+#include <asio/io_context.hpp>
+#include <asio/ip/tcp.hpp>
+#include <asio/write.hpp>
+
+#include <ctime>
+#include <iostream>
+#include <string>
+
+std::string make_daytime_string() {
+  time_t now = time(nullptr);
+  return ctime(&now);
+}
+
+int main(int argc, char** argv) {
+  using namespace asio::ip;
+  try {
+    if (argc != 2) {
+      fmt::print(std::cerr, "Usage:daytime2 <port>\n");
+      return 1;
+    }
+    asio::io_context io_ctx;
+    asio::ip::tcp::acceptor acceptor(
+        io_ctx, tcp::endpoint(tcp::v4(), std::stoi(argv[1])));
+
+    while (true) {
+      tcp::socket socket(io_ctx);
+      acceptor.accept(socket);
+
+      std::string message = make_daytime_string();
+      asio::error_code ignored_error;
+      asio::write(socket, asio::buffer(message), ignored_error);
+    }
+  } catch (const std::exception& e) {
+    fmt::print(std::cerr, "{}\n", e.what());
+  }
+
+  return 0;
+}
