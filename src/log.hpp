04eb6fcee3bca0854b172ccb7c01e9b75325b9b5
maidamai0 <yuantonghao@gmail.com>
2021-07-04 12:13:22 +0800

    chore: :loud_sound: make log colorful

--- src/log.hpp
+++ src/log.hpp
@@ -35,17 +35,17 @@ class Log {
     auto std_cout_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
 
     spdlog::set_default_logger(
-        std::make_shared<spdlog::logger>("OpenGL", spdlog::sinks_init_list{std_cout_sink, file_sink}));
+        std::make_shared<spdlog::logger>("LearnAsio", spdlog::sinks_init_list{std_cout_sink, file_sink}));
 
     spdlog::set_level(spdlog::level::debug);
     // spdlog::enable_backtrace(10);
     spdlog::flush_on(spdlog::level::debug);
-    spdlog::set_pattern("%Y-%m-%d %T.%e %L %v @%s:%#");
+    spdlog::set_pattern("%Y-%m-%d %T.%e %L %^%v%$ @%s:%#");
 
-    SPDLOG_INFO(fmt::format("Application {} started", APP_NAME));
+    LOGI("Application {} started", APP_NAME);
   }
 
-  ~Log() { SPDLOG_INFO(fmt::format("Application {} terminated", APP_NAME)); }
+  ~Log() { LOGI("Application {} terminated", APP_NAME); }
 };
 
 const Log log;
