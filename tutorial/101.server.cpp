bab0bfe7be9dd63bce4e434f383bb3cdcc32d678
maidamai0 <yuantonghao@gmail.com>
2021-08-30 22:19:12 +0800

    feat: tutorial 101 a simple server

---
+++ tutorial/101.server.cpp
@@ -0,0 +1,31 @@
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+
+#include "asio/io_context.hpp"
+#include "asio/ip/tcp.hpp"
+#include "asio/write.hpp"
+#include "cxxopts.hpp"
+
+int main(int argc, char* argv[]) {
+  int port = 5678;
+  if (argc > 1) {
+    port = std::atoi(argv[1]);
+  }
+
+  try {
+    asio::io_context io_context;
+    asio::ip::tcp::acceptor acceptor(io_context,
+                                     asio::ip::tcp::endpoint(asio::ip::tcp::v4(), port));
+    std::cout << "server running on " << port << '\n';
+    while (true) {
+      asio::ip::tcp::socket socket(io_context);
+      acceptor.accept(socket);
+      asio::write(socket, asio::buffer("Hello, world!\n"));
+    }
+  } catch (const std::exception& e) {
+    std::cerr << "exception: " << e.what() << "\n";
+  }
+
+  return 0;
+}
