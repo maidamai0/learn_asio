1adde76a9db0f58fcddb1c42f150abeef9f0be40
tonghao.yuan <tonghao.yuan@csdental.com>
2021-09-08 09:30:30 +0800

    feat: async tcp server with streambuf

--- tutorial/202.async_tcp_server_streambuf.cpp
+++ tutorial/202.async_tcp_server_streambuf.cpp
@@ -8,7 +8,9 @@
 #include "asio/streambuf.hpp"
 #include "asio/write.hpp"
 
+#include <deque>
 #include <iostream>
+#include <string>
 
 using socket_t = asio::ip::tcp::socket;
 using buffer_t = asio::streambuf;
@@ -17,6 +19,7 @@ using port_t = asio::ip::port_type;
 class session : public std::enable_shared_from_this<session> {
  public:
   session(asio::io_context& io_context) : socket_(io_context) {}
+  ~session() { std::cout << name() << __FUNCTION__ << std::endl; }
   socket_t& socket() { return socket_; }
   void start() { read(); }
 
@@ -28,9 +31,9 @@ class session : public std::enable_shared_from_this<session> {
                            });
   }
 
-  void write(const std::string& message) {
+  void write() {
     asio::async_write(
-        socket_, asio::buffer(message),
+        socket_, asio::buffer(messages_.front()),
         [self = shared_from_this()](const asio::error_code& ec, size_t bytes_transferred) {
           self->on_write(ec, bytes_transferred);
         });
@@ -44,36 +47,61 @@ class session : public std::enable_shared_from_this<session> {
     std::string line;
     std::istream is(&buffer_);
     std::getline(is, line);
-    write(std::move(line));
+    std::cout << name() << line << std::endl;
     buffer_.consume(bytes_transferred);
+
+    const auto write_idle = messages_.empty();
+    messages_.push_back(line);
+    if (write_idle) {
+      write();
+    }
+
     read();
   }
 
   void on_write(const asio::error_code& ec, std::size_t bytes_transferred) {
+    (void)bytes_transferred;
     if (ec) {
       return on_error(ec);
     }
 
-    (void)bytes_transferred;
+    messages_.pop_front();
+    if (!messages_.empty()) {
+      write();
+    }
   }
 
   void on_error(const asio::error_code& ec) {
+    std::cerr << name() << ec.value() << ", " << ec.message() << std::endl;
     socket_.close();
-    std::cerr << "Error: " << ec.message() << ", closed" << std::endl;
+  }
+
+  std::string name() const {
+    std::stringstream ss;
+    if (socket_.is_open()) {
+      ss << socket_.remote_endpoint() << ": ";
+    }
+
+    return ss.str();
   }
 
  private:
   socket_t socket_;
   buffer_t buffer_;
+  std::deque<std::string> messages_;
 };
 using session_ptr = std::shared_ptr<session>;
 
 class server {
  public:
-  server(port_t port)
+  server(port_t port = 6789)
       : port_(port), acceptor_(io_context_, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), port_)) {}
 
-  void run() { accept(); }
+  void run() {
+    accept();
+    std::cout << "Server started on port " << port_ << std::endl;
+    io_context_.run();
+  }
 
  private:
   void accept() {
@@ -97,5 +125,7 @@ class server {
 };
 
 int main() {
+  server s;
+  s.run();
   return 0;
 }
